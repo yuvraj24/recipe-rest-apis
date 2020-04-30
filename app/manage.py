0288b0cf160b2d36c00105143e42ac258bccbd5a
Yuvraj Pandey <yuvrajpandey24@gmail.com>
2020-04-30 23:27:12 +0530

    Django Setup

--- app/manage.py
+++ app/manage.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
-import sys
-
-
+import sys 
+ 
+ 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
     try:
