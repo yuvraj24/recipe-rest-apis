e72f95f4c17f5ef5558864f13bb7a48ce2b6f3e6
Yuvraj Pandey <yuvrajpandey24@gmail.com>
2020-05-02 23:18:06 +0530

    Manager Users Api Added

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import CreateUserView, CreateTokenView
+from .views import CreateUserView, CreateTokenView, ManageUserViews
 
 app_name = 'user'
 
 urlpatterns = [
     path('create/', CreateUserView.as_view(), name='create'),
     path('token/', CreateTokenView.as_view(), name='token'),
+    path('me/', ManageUserViews.as_view(), name='me'),
 ]
