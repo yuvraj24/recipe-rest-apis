437d8a4ba2d33c79485250ecd4614d824a545983
Yuvraj Pandey <yuvrajpandey24@gmail.com>
2020-05-02 23:21:59 +0530

    Manager Users Api Added

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -26,6 +26,7 @@ class UserSerializer(serializers.ModelSerializer):
 
         return user
 
+
 class AuthTokenSerializer(serializers.Serializer):
     # Serializer for the user authentication object
     email = serializers.CharField()
